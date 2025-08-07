d6695be103656f79522cb6d927fda4fab1c2ace8
Abdelilah ETTARCH <125505938+AbdelilahXR@users.noreply.github.com>
2025-08-07 16:51:21 +0100

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
-import { DM_Sans } from "next/font/google";
+import { DM_Sans, Inter } from "next/font/google";
 import clsx from "clsx";
 import "./globals.css";
+
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
