916be6bdaffe27c298112213847bc33acaefc1c6
Abdelilah ETTARCH <125505938+AbdelilahXR@users.noreply.github.com>
2025-08-07 18:24:50 +0100

    Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,14 @@
 export const Banner = () => {
-  return null;
-};
+  return (
+    <div className="py-3 text-center bg-[linear-gradient(to_right,rgb(252,_214,_255.7),rgb(41,_216,_255.7),rgb(255,_253,_128.7),rgb(248,_155,_192.7),#FCD6FF)]">
+        <div className="container">
+            <p>
+                
+            </p>
+            <span>Introducing a completely redesigned interface - </span>
+            <a href="#" className="underline underline-offset-4 font-medium">Explore the demo</a>
+        </div>
+    </div>
+   
+  )
+};  
