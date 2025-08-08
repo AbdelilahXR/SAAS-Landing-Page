5820698344362e3a1893a5a200e03c2570921c6d
Abdelilah ETTARCH <125505938+AbdelilahXR@users.noreply.github.com>
2025-08-08 16:28:24 +0100

    Banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,11 +2,11 @@ export const Banner = () => {
   return (
     <div className="py-3 text-center bg-[linear-gradient(to_right,rgb(252,_214,_255.7),rgb(41,_216,_255.7),rgb(255,_253,_128.7),rgb(248,_155,_192.7),#FCD6FF)]">
         <div className="container">
-            <p>
-                
+            <p className="font-medium">
+                <span className="hidden sm:inline">Introducing a completely redesigned interface - </span>
+                <a href="#" className="underline underline-offset-4 font-medium">Explore the demo</a>
             </p>
-            <span>Introducing a completely redesigned interface - </span>
-            <a href="#" className="underline underline-offset-4 font-medium">Explore the demo</a>
+            
         </div>
     </div>
    
